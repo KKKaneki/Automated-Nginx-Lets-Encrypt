9fbd7ae056bbde54bc7e58911f2e368a014329ad
pradyumnap <pradyumnaraje.patil@gmail.com>
2021-02-13 21:49:25 +0530

    ERROR HANDLING IN SCRIPT

--- run.sh
+++ run.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-# DOWNLOAD CERTBOT
-sudo apt install certbot python3-certbot-nginx
 
-# IF FIREWALL IS ENABLED ON THE MACHINE ALLOW NGINX
-sudo ufw allow "Nginx Full"
+if [ $# -eq 0];
+then
+    echo "Domain Name argument not received"
+    exit 1
+else
+    # DOWNLOAD CERTBOT
+    sudo apt install certbot python3-certbot-nginx
 
-# CREATE A SSL CERTIFICATE
-sudo certbot --nginx -d $1 -d www.$1
+    # IF FIREWALL IS ENABLED ON THE MACHINE ALLOW NGINX
+    sudo ufw allow "Nginx Full"
 
-echo "Congratulation HTTPS is enabled for $1"
+    # CREATE A SSL CERTIFICATE
+    sudo certbot --nginx -d $1 -d www.$1 && echo "Congratulation HTTPS is enabled for $1" || echo "Make sure your nginx has a domain"
+    exit 0
+fi
